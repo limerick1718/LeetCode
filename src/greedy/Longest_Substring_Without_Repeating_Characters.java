82b37faeebfdcc46e846441165947fe4b870fc03
limerick <limerick1718@sina.com>
2017-12-06 20:43:09 +0800

    Longest_Substring_Without_Repeating_Characters

--- src/greedy/Longest_Substring_Without_Repeating_Characters.java
+++ src/greedy/Longest_Substring_Without_Repeating_Characters.java
@@ -1,47 +1,54 @@
 package greedy;
 
-import javax.imageio.ImageTypeSpecifier;
+import java.util.HashMap;
 
+import javax.swing.text.html.HTML;
+
+/***
+ * Given a string, find the length of the longest substring without repeating characters.
+ * Examples:
+ * Given "abcabcbb", the answer is "abc", which the length is 3.
+ * Given "bbbbb", the answer is "b", with the length of 1.
+ * Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, 
+ * "pwke" is a subsequence and not a substring.
+ * @author limerick
+ *
+ */
 public class Longest_Substring_Without_Repeating_Characters {
 
 	public int lengthOfLongestSubstring(String s) {
-		int[] map = new int[256];
-		int counter = 0;
-		for (int i = 0; i < s.length(); i++) {
-			if (map[s.charAt(i)] == 0) {
-				map[s.charAt(i)]++;
-				counter ++;
+		char[] charArray = s.toCharArray();
+		HashMap<Character, Integer> hashMap = new HashMap<>();
+		int left = 0;
+		int right = -1;
+		int max = -1;
+		for (int i = 0; i < charArray.length; i ++) {
+			right = i;
+			if (hashMap.containsKey(charArray[i])) {
+				left = hashMap.get(charArray[i]);
+				hashMap.remove(charArray[i]);
+				max = Math.max(max, right - left);
 			}
+			hashMap.put(charArray[i], i);
+			max = Math.max(max, right - left);
 		}
-		int left = 0, right = 0, begin = 0, end = 0;
-		int max = Integer.MIN_VALUE;
-		while (left < s.length()) {
-			if (map[s.charAt(right ++)] -- == 0) {
-				counter--;
-			}
-			while (counter == 0) {
-				if (right - left >  max) {
-					max = right - left;
-					begin = left;
-					end = right;
-				}
-				if (map[s.charAt(left ++)] ++ == 1) {
-					counter ++;
-				}
-			}
-		}
-		if (max == Integer.MIN_VALUE) {
-			return s.length();
-		}
-		else {
-			return end - begin;
-		}
-
+		max = Math.max(max, right - left);
+		if (left == -1) return hashMap.size();
+		return max;
 	}
-
+	/***
+	 * "aab"
+	 * "a"
+	 * "abcabcbb"
+	 * "bbbbb"
+	 * "pwwkew"
+	 * "ccb"
+	 * "abba"
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		Longest_Substring_Without_Repeating_Characters longest_Substring_Without_Repeating_Characters = new Longest_Substring_Without_Repeating_Characters();
-		int lengthOfLongestSubstring = longest_Substring_Without_Repeating_Characters.lengthOfLongestSubstring("abcabcbb");
+		int lengthOfLongestSubstring = longest_Substring_Without_Repeating_Characters.lengthOfLongestSubstring("abba");
 		System.out.println(lengthOfLongestSubstring);
 	}
 
